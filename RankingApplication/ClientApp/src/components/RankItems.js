14d2c954a4988fb3b59e70be4d4602a92c74cca5
Ivan Iliev <ivanilievdackel@gmail.com>
2023-04-26 00:55:12 +0300

    start of SPA demo

---
+++ RankingApplication/ClientApp/src/components/RankItems.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import MovieImageArr from "./MovieImages.js";
+import RankingGrid from './RankingGrid.js';
+
+const RankItems = () => {
+
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+                setItems(data);
+            })
+    }, [])
+
+    return (
+        <main>
+            <RankingGrid items={items} imgArr={MovieImageArr} />
+            <div className="items-not-ranked">
+                {
+                    (items.length != 0) ? items.map((item) =>
+                        <div className = "unranked-cell">
+                            <img id={`item${item.id}`} src={MovieImageArr.find(x => x.id === item.imageId)?.image} />
+                        </div>
+
+                    ) : <div>Loading...</div>
+                }
+            </div>
+        </main>
+    )
+}
+export default RankItems;
